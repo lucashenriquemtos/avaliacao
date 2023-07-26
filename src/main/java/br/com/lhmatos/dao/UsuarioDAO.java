8a81d00c9ae5443acf3b35ac2ef8a1f7b50436f0
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-26 15:38:59 -0300

    Adicionando UsuarioDAO e testando conexão na classe main

---
+++ src/main/java/br/com/lhmatos/dao/UsuarioDAO.java
@@ -0,0 +1,23 @@
+package br.com.lhmatos.dao;
+
+public class UsuarioDAO extends BaseDAO{
+	@Override
+	public Object create(Object o) {
+		return null;
+	}
+
+	@Override
+	public Object read(String id) {
+		return null;
+	}
+
+	@Override
+	public Object update(Object o) {
+		return null;
+	}
+
+	@Override
+	public boolean delete(String id) {
+		return false;
+	}
+}
