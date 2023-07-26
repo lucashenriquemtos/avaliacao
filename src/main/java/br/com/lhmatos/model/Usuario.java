fb2454b68ca9f8aa4b45ab96ffffe8a46efb8388
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-26 17:11:13 -0300

    Adicionando validação na classe Usuario para permitir no minimo 1 e no máximo 90

--- src/main/java/br/com/lhmatos/model/Usuario.java
+++ src/main/java/br/com/lhmatos/model/Usuario.java
@@ -1,11 +1,17 @@
 package br.com.lhmatos.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
+
 
 @Data
 public class Usuario {
 
 	private final String nmLogin;
 	private final String dsSenha;
+
+	@Min(1)
+	@Max(90)
 	private final Integer qtTempoInatividade;
 }
