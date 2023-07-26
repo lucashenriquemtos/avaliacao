cd5bc2731fd477162dab74ea9e1d8206b6a673b4
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-26 18:37:17 -0300

    Atualizando UsuarioDTO

--- src/main/java/br/com/lhmatos/dto/UsuarioDTO.java
+++ src/main/java/br/com/lhmatos/dto/UsuarioDTO.java
@@ -1,13 +1,35 @@
 package br.com.lhmatos.dto;
 
+import br.com.lhmatos.model.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
 	private String nmLogin;
 	private String dsSenha;
 	private Integer qtTempoInatividade;
+
+	public Usuario toUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNmLogin(this.nmLogin);
+		usuario.setDsSenha(this.dsSenha);
+		usuario.setQtTempoInatividade(this.qtTempoInatividade);
+		return usuario;
+	}
+
+	public static UsuarioDTO fromUsuario(Usuario usuario) {
+		UsuarioDTO dto = new UsuarioDTO();
+		dto.setNmLogin(usuario.getNmLogin());
+		dto.setDsSenha(usuario.getDsSenha());
+		dto.setQtTempoInatividade(usuario.getQtTempoInatividade());
+		return dto;
+	}
+
+
+
 }
