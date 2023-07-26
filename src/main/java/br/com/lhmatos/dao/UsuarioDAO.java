d1fcb5cbb511e98197a9363874306d329eace08e
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-26 15:50:58 -0300

    Adicionando imports e atualizando método Create

--- src/main/java/br/com/lhmatos/dao/UsuarioDAO.java
+++ src/main/java/br/com/lhmatos/dao/UsuarioDAO.java
@@ -1,8 +1,29 @@
 package br.com.lhmatos.dao;
 
-public class UsuarioDAO extends BaseDAO{
+import br.com.lhmatos.dto.UsuarioDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsuarioDAO extends BaseDAO<UsuarioDTO> {
+
 	@Override
-	public Object create(Object o) {
+	public UsuarioDTO create(UsuarioDTO usuarioDTO) {
+		String sql = "INSERT INTO usuario (nm_login, ds_senha, qt_tempo_inatividade) VALUES (?, ?, ?)";
+		try (Connection conn = getConnection();
+			 PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, usuarioDTO.getNmLogin());
+			stmt.setString(2, usuarioDTO.getDsSenha());
+			stmt.setInt(3, usuarioDTO.getQtTempoInatividade());
+			int affectedRows = stmt.executeUpdate();
+			if (affectedRows > 0) {
+				return usuarioDTO;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
