975468da9ba9cfb3cd45e7e4a8b4fc7a635e435c
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-26 17:09:51 -0300

    Corrigindo DAO para tratar com Usuario.java

--- src/main/java/br/com/lhmatos/dao/UsuarioDAO.java
+++ src/main/java/br/com/lhmatos/dao/UsuarioDAO.java
@@ -1,25 +1,25 @@
 package br.com.lhmatos.dao;
 
-import br.com.lhmatos.dto.UsuarioDTO;
+import br.com.lhmatos.model.Usuario;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UsuarioDAO extends BaseDAO<UsuarioDTO> {
+public class UsuarioDAO extends BaseDAO<Usuario> {
 
 	@Override
-	public UsuarioDTO create(UsuarioDTO usuarioDTO) {
+	public Usuario create(Usuario usuario) {
 		String sql = "INSERT INTO usuario (nm_login, ds_senha, qt_tempo_inatividade) VALUES (?, ?, ?)";
 		try (Connection conn = getConnection();
 			 PreparedStatement stmt = conn.prepareStatement(sql)) {
-			stmt.setString(1, usuarioDTO.getNmLogin());
-			stmt.setString(2, usuarioDTO.getDsSenha());
-			stmt.setInt(3, usuarioDTO.getQtTempoInatividade());
+			stmt.setString(1, usuario.getNmLogin());
+			stmt.setString(2, usuario.getDsSenha());
+			stmt.setInt(3, usuario.getQtTempoInatividade());
 			int affectedRows = stmt.executeUpdate();
 			if (affectedRows > 0) {
-				return usuarioDTO;
+				return usuario;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
