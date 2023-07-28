ef98c8ff2cc8713b3fcf265261e152054a07c145
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-07-28 16:48:52 -0300

    Adicionando create

--- src/main/java/br/com/lhmatos/dao/ExameRealizadoDAO.java
+++ src/main/java/br/com/lhmatos/dao/ExameRealizadoDAO.java
@@ -2,12 +2,26 @@ package br.com.lhmatos.dao;
 
 import br.com.lhmatos.model.ExameRealizado;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class ExameRealizadoDAO extends BaseDAO<ExameRealizado> {
 	@Override
 	public ExameRealizado create(ExameRealizado exameRealizado) {
-		return null;
+		String sql = "INSERT INTO exame_realizado(cd_funcionario, cd_exame, dt_realizacao) VALUES (?, ?, ?)";
+
+		try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, exameRealizado.getCdFuncionario());
+			stmt.setInt(2, exameRealizado.getCdExame());
+			stmt.setDate(3, Date.valueOf(exameRealizado.getDtRealizacao()));
+			stmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return exameRealizado;
 	}
 
 	@Override
