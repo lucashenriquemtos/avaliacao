7009ef8de198cee86a0a3c5ac04261ea4756eb82
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-08-01 18:37:49 -0300

    Adicionando ExameService

---
+++ src/main/java/br/com/lhmatos/webservice/ExameService.java
@@ -0,0 +1,6 @@
+package br.com.lhmatos.webservice;
+
+import br.com.lhmatos.dto.ExameDTO;
+
+public interface ExameService extends BaseService<ExameDTO> {
+}
---
+++ src/main/java/br/com/lhmatos/webservice/ExameServiceImpl.java
@@ -0,0 +1,70 @@
+package br.com.lhmatos.webservice;
+
+import br.com.lhmatos.dao.ExameDAO;
+import br.com.lhmatos.dto.ExameDTO;
+import br.com.lhmatos.model.Exame;
+import jakarta.jws.WebService;
+
+import java.util.Optional;
+
+@WebService
+public class ExameServiceImpl implements ExameService {
+
+	private ExameDAO exameDAO = new ExameDAO();
+
+	public ExameServiceImpl() { }
+
+	public ExameServiceImpl(ExameDAO exameDAO) { this.exameDAO = exameDAO; }
+
+	@Override
+	public ExameDTO create(ExameDTO dto) {
+		try {
+			Exame exame = dto.toExame();
+			Exame createdExame = exameDAO.create(exame);
+			if(createdExame != null) {
+				return ExameDTO.fromExame(createdExame);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	@Override
+	public ExameDTO findById(String id) {
+		try {
+			Optional<Exame> exameOptional = exameDAO.findById(id);
+			if(exameOptional.isPresent()) {
+				return ExameDTO.fromExame(exameOptional.get());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public ExameDTO update(ExameDTO dto) {
+		try {
+			Exame exame = dto.toExame();
+			Exame updatedExame = exameDAO.update(exame);
+			if(updatedExame != null) {
+				return ExameDTO.fromExame(updatedExame);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteByLogin(String id) {
+		try {
+			return exameDAO.delete(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
