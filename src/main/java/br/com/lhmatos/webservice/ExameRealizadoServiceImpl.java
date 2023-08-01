100c016b73a9f3000ae89c5108a301019548a7c0
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2023-08-01 19:32:51 -0300

    Adicionando ExameRealizadoServiceImpl

---
+++ src/main/java/br/com/lhmatos/webservice/ExameRealizadoServiceImpl.java
@@ -0,0 +1,32 @@
+package br.com.lhmatos.webservice;
+
+import br.com.lhmatos.dto.ExameDTO;
+
+import java.util.List;
+
+public class ExameRealizadoServiceImpl implements ExameService{
+	@Override
+	public ExameDTO create(ExameDTO dto) {
+		return null;
+	}
+
+	@Override
+	public ExameDTO findById(String id) {
+		return null;
+	}
+
+	@Override
+	public ExameDTO update(ExameDTO dto) {
+		return null;
+	}
+
+	@Override
+	public boolean delete(String id) {
+		return false;
+	}
+
+	@Override
+	public List<ExameDTO> findByCdOrNmOrIc(String cdExame, String nmExame, boolean icAtivo) {
+		return null;
+	}
+}
